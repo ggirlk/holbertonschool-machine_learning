fbd3b3bc8dce90317aa6dc64410e4e583fe301ac
= <=>
2020-12-02 00:56:20 +0100

    Ridin’ Bareback

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def mat_mul(mat1, mat2):
+    mat = []
+    for i in range(0, len(mat1)):
+        res = []
+        for j in range(0, len(mat2[0])):
+            sm = 0
+            for k in range(0, len(mat2)):
+                sm += mat1[i][k] * mat2[k][j]
+            res.append(sm)
+        mat.append(res)
+    return(mat)
