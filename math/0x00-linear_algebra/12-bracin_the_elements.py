822e9a213463388c018f49e2272af6860e971cbd
= <=>
2020-12-02 01:43:00 +0100

    add doc

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" element-wise addition, subtraction, multiplication, division """
 
 
 def np_elementwise(mat1, mat2):
+    """ element-wise addition, subtraction, multiplication, division """
     import numpy as np
     return [np.add(mat1, mat2), np.subtract(mat1, mat2),
             np.multiply(mat1, mat2), np.divide(mat1, mat2)]
