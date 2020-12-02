6474f9832d2dd6905e9128a172fbcb4a62789431
= <=>
2020-12-02 02:43:21 +0100

    Gettin’ Cozy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,8 +9,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
         mat = mat1 + mat2
         return copy.deepcopy(mat)
     if axis == 1:
-        if len(mat1[0]) != len(mat2):
-            return None
+        """if len(mat1[0]) != len(mat2):
+            return None"""
         mat = mat1[:]
         for i in range(0, len(mat2)):
             for j in mat2[i]:
