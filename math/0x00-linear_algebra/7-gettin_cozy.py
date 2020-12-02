4ca8c18f05976f6c458635e9d89ea77a9b6c2936
= <=>
2020-12-02 19:00:00 +0100

    cat

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,12 +5,17 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenate """
     import copy
+    
     if axis == 0:
+        if (len(mat1) != len(mat2[0])):
+            return None
         mat = mat1 + mat2
         return mat
     if axis == 1:
+        if (len(mat1[0]) != len(mat2)):
+            return None
         mat = copy.deepcopy(mat1)
-        for i in range(0, len(mat2)):
+        for i in range(0, len(mat2)):            
             for j in mat2[i]:
                 mat[i].append(j)
         return mat
