8a3106bb27ca45f01feb56ee03fda70b8a80fc3d
= <=>
2020-12-02 17:48:14 +0100

    add

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -2,26 +2,14 @@
 """ add two arrays element-wise """
 
 
-def shaperec(matrix, shapelist=[]):
-    """ recursion for the shape """
-    if type(matrix) == list:
-        shapelist.append(len(matrix))
-        shaperec(matrix[0], shapelist)
-
-
-def matrix_shape(matrix):
-    """ matrix shape """
-    shapelist = []
-    shaperec(matrix, shapelist)
-    return shapelist
-
 
 def add_arrays(arr1, arr2):
     """ add """
-    if (matrix_shape(arr1) != matrix_shape(arr2)):
-        return(None)
-    new = []
-    for i in range(0, len(arr1)):
-        sm = arr1[i] + arr2[i]
-        new.append(sm)
-    return(new)
+    try:
+        new = []
+        for i in range(0, len(arr1)):
+            sm = arr1[i] + arr2[i]
+            new.append(sm)
+        return(new)
+    except:
+        return None
