6f5775a888f74a5c0044802cd27bdbfd5584f540
= <=>
2020-12-02 01:25:35 +0100

    add doc

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
+""" add two arrays element-wise """
+
+
 def shaperec(matrix, shapelist=[]):
+    """ recursion for the shape """
     if type(matrix) == list:
         shapelist.append(len(matrix))
         shaperec(matrix[0], shapelist)
 
 
 def matrix_shape(matrix):
+    """ matrix shape """
     shapelist = []
     shaperec(matrix, shapelist)
     return shapelist
 
 
 def add_arrays(arr1, arr2):
+    """ add """
     if (matrix_shape(arr1) != matrix_shape(arr2)):
         return(None)
     new = []
