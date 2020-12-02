a239e019e3ae599ebfaf3fbcc1f1aef85666056d
= <=>
2020-12-02 18:27:27 +0100

    mat mul

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,13 +4,16 @@
 
 def mat_mul(mat1, mat2):
     """ matrix multiplication """
-    mat = []
-    for i in range(0, len(mat1)):
-        res = []
-        for j in range(0, len(mat2[0])):
-            sm = 0
-            for k in range(0, len(mat2)):
-                sm += mat1[i][k] * mat2[k][j]
-            res.append(sm)
-        mat.append(res)
-    return(mat)
+    try:
+        mat = []
+        for i in range(0, len(mat1)):
+            res = []
+            for j in range(0, len(mat2[0])):
+                sm = 0
+                for k in range(0, len(mat2)):
+                    sm += mat1[i][k] * mat2[k][j]
+                res.append(sm)
+            mat.append(res)
+        return(mat)
+    except Exception:
+        return None
