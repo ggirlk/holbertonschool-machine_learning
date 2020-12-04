88ed8024b94e13a469922b671de91d6b1adbbc2b
= <=>
2020-12-04 02:33:42 +0100

    slice

--- math/0x00-linear_algebra/.ipynb_checkpoints/100-slice_like_a_ninja-checkpoint.py
+++ math/0x00-linear_algebra/.ipynb_checkpoints/100-slice_like_a_ninja-checkpoint.py
@@ -4,26 +4,11 @@
 
 def np_slice(matrix, axes={}):
     """ slice function"""
-    axs = list(axes.keys())
-    nmatrix = matrix[:]
     new = []
-    for i in axs:
-        start = axes[i][0]        
-        if len(axes[i]) > 1:
-            stop = axes[i][1]
+    for i in range(0, len(matrix.shape)):
+        if i not in axes:
+            sl = slice(None, None, None)
         else:
-            stop = None
-        if len(axes[i]) > 2:
-            step = axes[i][2]
-        else:
-            step = None
-        if i == 0:
-            sl = slice (start)
-            nmatrix = nmatrix[sl]
-        if (i == 1):
-            sl = slice (start, stop, i)
-            nmatrix = nmatrix[:,sl]
-        if (i == 2):
-            sl = slice (start, stop, i)
-            nmatrix = nmatrix[:,:,sl]
-    return nmatrix
+            sl = slice(*axes[i])
+        new.append(sl)
+    return matrix[tuple(new)]
