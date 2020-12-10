bd5024cabc08ea8968cd268eb0ac4764560ec198
= <ggirlk2@gmail.com>
2020-12-10 03:01:12 +0100

    summation n squared

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,10 +4,9 @@
 
 def summation_i_squared(n):
     """ summation n squared """
-    if type(n) != int:
+    if type(n) != int or n <= 0:
         return None
-    if n < 0:
-        n *= -1
+    
     ls = list(range(1, n+1))
     s = sum(map(lambda x: pow(x, 2), ls))
     return s
