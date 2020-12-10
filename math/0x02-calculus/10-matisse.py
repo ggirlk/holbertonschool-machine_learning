15d8414c986f022ca59a4faa1c8ef70d4b52e3d1
= <ggirlk2@gmail.com>
2020-12-10 18:26:35 +0100

    deriv

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,15 +4,14 @@
 
 def poly_derivative(poly):
     """ doc """
-    if len(poly) == 0 or type(poly) != list:
+    if len(poly) == 0 or type(poly) is not list:
         return None
+    if len(poly) == 1:
+        return [0]
 
-    drv = [0]
-    for i in range(1, len(poly)):
+    for i in range(0, len(poly)):
         if not isinstance(poly[i], (int, float)):
             return None
-        drv.append(poly[i] * i)
-    if sum(drv) == 0:
-        return [0]
+        drv.append(poly[i] * i) 
     del drv[0]
     return drv
