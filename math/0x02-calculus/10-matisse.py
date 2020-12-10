8adcd0f8727a8885f4c2afa0dea472370f25cba0
= <ggirlk2@gmail.com>
2020-12-10 03:42:13 +0100

    deriv

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,6 +6,8 @@ def poly_derivative(poly):
     """ doc """
     if type(poly) != list:
         None
+    if len(poly) == 1:
+        return [0]
     drv = []
     for i in range(1, len(poly)):
         drv.append(poly[i] * i)
