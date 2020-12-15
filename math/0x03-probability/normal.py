19d1ec3ba72e48d4da3f7f409609365b6b96c142
= <ggirlk2@gmail.com>
2020-12-15 07:02:58 +0100

    add distribution classes

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,57 @@
+""" class Normal """
+
+
+class Normal():
+    """ Represents a normal distribution """
+    
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ constractor """
+        if data == None:
+            if stddev < 0:
+                raise ValueError('stddev must be a positive value')
+            self.stddev = stddev
+            self.mean = mean
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) <= 2:
+                raise ValueError('data must contain multiple values')            
+            self.mean = sum(data)/len(data)
+            s = 0
+            for x in data:
+                s += (x - self.mean)**2
+            self.stddev = (s/len(data))**0.5
+
+    def z_score(self, x):
+        """
+            Calculates the z-score
+            of a given x-value
+        """
+        return (x - self.mean)/self.stddev
+
+    def x_value(self, z):
+        """
+            Calculates the x-value
+            of a given z-score
+        """
+        return z*self.stddev + self.mean
+
+    def pdf(self, x):
+        """
+            Calculates the value of the PDF
+            for a given x-value
+        """
+        return 2.7182818285**-((x-self.mean)**2/(2*self.stddev**2))/(self.stddev *(2*3.1415926536)**0.5)
+
+    def cdf(self, x):
+        """
+            Calculates the value of the CDF
+            for a given x-value
+        """
+        mu = self.mean
+        std = self.stddev
+        def erf(a):
+            """ Error Function """
+            return 2*(a-((a**3)/3)+((a**5)/10)-((a**7)/42)+((a**9)/216))/(3.1415926536**0.5)
+        n = ((x - mu) / std) / (2**0.5)
+        return (1 + erf(n)) / 2
---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" class Poisson """
+
+
+class Poisson():
+    """ Represent a poisson distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ constractor """        
+        if data == None:
+            if lambtha < 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) <= 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = sum(data)/len(data)
+
+    def pmf(self, k):
+        """
+            Calculates the value of the PMF
+            for a given number of “successes”
+        """
+        try:
+            k = int(k)
+            if k < 0:
+                return 0
+            lmda = self.lambtha
+            fact = 1
+            for i in range(1, k+1): 
+                fact = fact * i
+            return 2.7182818285**-lmda*lmda**k/fact
+        except:
+            return 0
+
+    def cdf(self, k):
+        """
+            Calculates the value of the CDF
+            for a given number of “successes”
+        """
+        try:
+            k = int(k)
+            if k < 0:
+                return 0
+            cdf = 0
+            for i in range(0, k+1):
+                cdf += self.pmf(i)
+            return cdf
+        except:
+            return 0
