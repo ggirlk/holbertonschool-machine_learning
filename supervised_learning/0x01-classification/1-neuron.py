a0f0a1c502b327bdb83bd34846921210a92568e6
= <ggirlk2@gmail.com>
2020-12-24 21:18:00 +0100

    happy neuron

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Class Neuron
+Define a single neuron performing binary classification
+"""
+import numpy as np
+
+
+class Neuron():
+    """ Class Neuron"""
+
+    def __init__(self, nx):
+        """ Constractor """
+
+        # nx: number of input features to the neuron
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        # weights vector for the neuron
+        self.__W = np.random.randn(1, self.nx)
+        # bias for the neuron
+        self.__b = 0
+        # activated output of the neuron (prediction)
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ weights getter """
+        return self.__W
+
+    @property
+    def b(self):
+        """ bias getter """
+        return self.__b
+
+    @property
+    def A(self):
+        """ active output getter """
+        return self.__A
---
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+Class Neuron
+Define a single neuron performing binary classification
+"""
+import numpy as np
+
+
+class Neuron():
+    """ Class Neuron """
+
+    def __init__(self, nx):
+        """ Constractor """
+
+        # nx: number of input features to the neuron
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        # weights vector for the neuron
+        self.__W = np.random.randn(1, self.nx)
+        # bias for the neuron
+        self.__b = 0
+        # activated output of the neuron (prediction)
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ weights getter """
+        return self.__W
+
+    @property
+    def b(self):
+        """ bias getter """
+        return self.__b
+
+    @property
+    def A(self):
+        """ active output getter """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+            Calculate the forward
+            propagation of the neuron
+            using sigmoid activation function
+        """
+
+        x = np.matmul(self.__W, X) + self.b
+        # sigmoid
+        self.__A = (1/(1+np.exp(-x)))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ Calculate the cost of the model using logistic regression """
+
+        m = A.shape[1]
+        s = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return -(1 / m) * s
+
+    def evaluate(self, X, Y):
+        """ Evaluate the neuron’s predictions """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        self.__A = np.where(self.forward_prop(X) >= 0.5, 1, 0)
+        return (self.__A, cost)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculate one pass of gradient descent on the neuron """
+        # The Mean Error
+        ME = np.mean((self.__A - Y))
+        # update bias
+        self.__b += ME * -alpha
+        # update weights
+        m = X.shape[1]
+        x = A - Y
+        xlr = -alpha/m * X.T
+        self.__W = np.add(self.__W, np.matmul(x, xlr))
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ train the neuron """
+        # check iterations validity
+        if type(iterations) is not int:
+            raise TypeError('iterations must be an integer')
+        if iterations < 0:
+            raise ValueError('iterations must be a positive integer')
+        # check alpha validity
+        if type(alpha) is not float:
+            raise TypeError('alpha must be a float')
+        if alpha < 0:
+            raise ValueError('alpha must be positive')
+        # train the model
+        for i in range(iterations):
+            self.__A = self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha)
+        self.__A, cost = self.evaluate(X, Y)
+        return (self.__A, cost)
