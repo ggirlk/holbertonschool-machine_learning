67515db4159f7a314a706f0f09d627778c9a5ab4
= <ggirlk2@gmail.com>
2020-12-30 15:59:10 +0100

    remove pyplot -_-

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -5,7 +5,6 @@ define a neural network with one hidden layer
 performing binary classification
 """
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class NeuralNetwork():
@@ -13,7 +12,7 @@ class NeuralNetwork():
 
     def __init__(self, nx, nodes):
         """ Constractor """
-        
+
         # nx: number of input features to the neuron
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -28,7 +27,7 @@ class NeuralNetwork():
         # weights vector for the hidden layer
         self.__W1 = np.random.randn(nodes, self.nx)
         # bias for the hidden layer
-        self.__b1 = np.array([[0]]* nodes)
+        self.__b1 = np.array([[0]] * nodes)
         # activated output of the hidden layer
         self.__A1 = 0
 
@@ -70,7 +69,7 @@ class NeuralNetwork():
     def A2(self):
         """ active output getter """
         return self.__A2
-    
+
     def sigmoid(self, X=None, w=None, b=None, x=None):
         """ sigmoid function """
         if (x is None):
@@ -80,11 +79,11 @@ class NeuralNetwork():
 
     def forward_prop(self, X):
         """
-            Calculate the forward 
+            Calculate the forward
             propagation of the neuron
             using sigmoid activation function
         """
-        
+
         # layer active output
         self.__A1 = self.sigmoid(X, self.W1, self.b1)
         # neuron active output
@@ -96,7 +95,7 @@ class NeuralNetwork():
         """ Calculate the cost of the model using logistic regression """
 
         m = Y.shape[1]
-        s = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) 
+        s = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return -(1 / m) * s
 
     def evaluate(self, X, Y):
@@ -105,12 +104,12 @@ class NeuralNetwork():
         cost = self.cost(Y, self.__A2)
         self.__A2 = np.where(self.__A2 >= 0.5, 1, 0)
         return (self.__A2, cost)
-    
+
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """ Calculate one pass of gradient descent on the neuron """
         m = X.shape[1]
         n = A1.shape[0]
-        
+
         def dw(dz, x):
             """ weight derivative """
             return np.matmul(dz, x.T)/m
@@ -118,23 +117,23 @@ class NeuralNetwork():
         def db(dz):
             """ bias derivative"""
             return np.mean(dz, axis=1, keepdims=True)
-        
+
         def der(x):
             """ sigmoid derivative """
             return x * (1-x)
-        
+
         def dz(w, dz, gprime):
             """ z derivative """
             x = gprime * np.matmul(w.T, dz)
             return x
-        
+
         # output neuron
         dz2 = A2 - Y
         dw2 = dw(dz2, A1)
         self.__W2 = np.add(self.W2, -alpha * dw2)
         db2 = db(dz2)
         self.__b2 += -alpha * db2
-        
+
         # hidden layer
         dz1 = dz(self.W2, dz2, der(A1))
         dw1 = dw(dz1, X)
@@ -164,9 +163,9 @@ class NeuralNetwork():
         for i in range(iterations):
             A1, A2 = self.forward_prop(X)
             self.gradient_descent(X, Y, A1, A2, alpha)
-            
+
         # evaluation of the training data after iterations
         self.__A2, cost = self.evaluate(X, Y)
-        
+
         # return the evaluation
         return (self.__A2, cost)
