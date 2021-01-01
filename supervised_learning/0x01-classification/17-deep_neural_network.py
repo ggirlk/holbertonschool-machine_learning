e058a7fe3d4f225091d48925b0fc427293e07be9
= <ggirlk2@gmail.com>
2021-01-01 15:30:36 +0100

    dnn privatize

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+class DeepNeuralNetwork
+define a deep neural network performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """ Class DeepNeuralNetwork """
+
+    def __init__(self, nx, layers):
+        """ Constractor """
+
+        # nx: number of input features to the neuron
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        # layers: number of nodes in each layer of the network
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        def check(b):
+            """ check layer list elements """
+            if type(b) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            if b < 1:
+                raise TypeError("layers must be a list of positive integers")
+        s = list(map(lambda b: check(b), layers))
+        # number of layers in the neural network
+        self.__L = len(layers)
+        # intermediary values of the network
+        self.__cache = {}
+        # weights and biased of the network
+        self.__weights = {}
+        for i in range(self.L):
+            n = layers[i]
+            if i == 0:
+                m = self.nx
+            else:
+                m = layers[i-1]
+            self.weights['W' + str(i+1)] = np.random.randn(n, m) * np.sqrt(2/m)
+            self.weights['b' + str(i+1)] = np.zeros((n, 1))
+
+    @property
+    def L(self):
+        """ number of layers L getter """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ cache getter """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ active output getter """
+        return self.__weights
