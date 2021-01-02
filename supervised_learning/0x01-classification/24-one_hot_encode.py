4a545a417f56d5a8f5f43e1ed276e2f7d017c612
= <ggirlk2@gmail.com>
2021-01-02 18:28:19 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ doc """
-    if type(Y) != np.ndarray and not Y:
+    if type(Y) != np.ndarray or type(classes) is not int:
         return None
     m = Y.shape[0]
     mx = np.zeros((classes, m))
