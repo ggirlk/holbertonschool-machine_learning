f3fb7c311693c7df0da8b73781bfc8969ef0d1ac
= <ggirlk2@gmail.com>
2021-01-02 17:53:41 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -3,10 +3,8 @@
 converts a numeric label
 vector into a one-hot matrix
 """
-import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    mx = np.zeros((classes, classes))
     
-    return mx
+    return Y
