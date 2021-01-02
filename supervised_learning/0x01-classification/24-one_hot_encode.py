3ed07ef76eace39ce48e07dca83dc9e40a9bdaa4
= <ggirlk2@gmail.com>
2021-01-02 23:24:49 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -11,6 +11,7 @@ def one_hot_encode(Y, classes):
     if type(Y) != np.ndarray\
        or type(classes) is not int\
        or len(Y) == 0\
+       or len(Y.shape) is not 1\
        or classes-1 != Y.max():
         return None
     m = Y.shape[0]
