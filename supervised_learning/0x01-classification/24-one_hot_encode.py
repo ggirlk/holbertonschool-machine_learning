684ebb26bf7b24142ab9fd6bbde5db6465f83b65
= <ggirlk2@gmail.com>
2021-01-02 17:51:10 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,8 +8,5 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     mx = np.zeros((classes, classes))
-    i = 0
-    for n in Y:
-        mx[n:n+1, i:i+1] = 1
-        i += 1
+    
     return mx
