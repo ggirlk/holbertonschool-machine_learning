b53c056656c0d5d0c18aed52624ec632a39b888d
= <ggirlk2@gmail.com>
2021-01-03 01:02:29 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -10,7 +10,6 @@ def one_hot_encode(Y, classes):
     """ doc """
     if type(Y) != np.ndarray\
        or type(classes) is not int\
-       or classes < 1\
        or len(Y) == 0\
        or len(Y.shape) != 1\
        or classes != Y.max()+1:
@@ -19,7 +18,7 @@ def one_hot_encode(Y, classes):
     mx = np.zeros((classes, m))
     i = 0
     for n in Y:
-        if type(n) is not np.int64 or n < 0:
+        if type(n) is not np.int64 or n is not np.int32:
             return None
         mx[n:n+1, i:i+1] = 1
         i += 1
