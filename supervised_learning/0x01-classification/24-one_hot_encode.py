9f1514c3ddfddfd438ec9984f1ba0582d051f4c1
= <ggirlk2@gmail.com>
2021-01-03 23:52:39 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,7 +8,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ doc """
-
+    if Y is None\
+       or type(Y) is not np.ndarray\
+       or type(classes) is not int:
+        return None
     try:
         return np.eye(classes)[Y].T
     except Exception:
