97048a520efa96c3f3e40e56afb1a9e847327a01
= <ggirlk2@gmail.com>
2021-01-04 00:21:01 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -9,17 +9,6 @@ import numpy as np
 def one_hot_decode(one_hot):
     """ doc """
     if type(one_hot) is not np.ndarray\
-       or len(one_hot) == 0\
        or len(one_hot.shape) != 2:
         return None
-    m = one_hot.shape[1]
-    mx = np.array([0]*m)
-    for i in range(len(one_hot)):
-        for j in range(len(one_hot[i])):
-            if one_hot[j][i] != 0 and one_hot[j][i] != 1:
-                return None
-            if one_hot[j][i] == 1:
-                mx[i] = j
-    if mx.max()+1 != one_hot.shape[0]:
-        return None
-    return mx
+    return np.array([np.where(i==1)[0][0] for i in one_hot.T])
