d27a910e1ff64acb42c1ba7344145658ae01fd55
= <ggirlk2@gmail.com>
2021-01-03 22:10:26 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -12,12 +12,6 @@ def one_hot_encode(Y, classes):
        or type(Y) is not np.ndarray\
        or type(classes) is not int\
        or len(Y) == 0\
-       or len(Y.shape) != 1\
        or classes-1 != Y.max():
         return None
-    mx = np.zeros((classes, Y.shape[0]))
-    i = 0
-    for n in Y:
-        mx[n:n+1, i:i+1] = 1
-        i += 1
-    return mx
+    return np.eye(classes)[Y].T
