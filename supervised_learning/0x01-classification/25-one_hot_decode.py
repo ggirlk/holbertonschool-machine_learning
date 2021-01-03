5ea72081cd87da497ab93d48527de0624f4e0f00
= <ggirlk2@gmail.com>
2021-01-04 00:25:44 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -11,4 +11,5 @@ def one_hot_decode(one_hot):
     if type(one_hot) is not np.ndarray\
        or len(one_hot.shape) != 2:
         return None
-    return np.array([np.where(i==1)[0][0] for i in one_hot.T])
+    return np.array([np.where(i == 1)[0][0]
+                     for i in one_hot.T])
