99cb7eeb4c77a9d3d95cc9c441b25d796386055d
= <ggirlk2@gmail.com>
2021-01-03 01:39:44 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -18,10 +18,8 @@ def one_hot_encode(Y, classes):
     mx = np.zeros((classes, m))
     i = 0
     for n in Y:
-        if type(n) is not np.int64:
+        if type(n) is not np.int64 or n < 0:
             return None
-        if n < 0:
-            n *= -1
         mx[n:n+1, i:i+1] = 1
         i += 1
     return mx
---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+converts a one-hot matrix
+into a vector of labels
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    if type(one_hot) is not np.ndarray:
+        return None
+    m = one_hot.shape[0]
+    mx = np.array([0]*m)
+    i = 0
+    for i in range(len(one_hot)):
+        for j in range(len(one_hot[i])):
+            if one_hot[j][i] != 0 and one_hot[j][i] != 1:
+                return None
+            if one_hot[j][i] == 1:
+                mx[i] = j
+    if mx.max()+1 != one_hot.shape[1]:
+        return None
+    return mx
