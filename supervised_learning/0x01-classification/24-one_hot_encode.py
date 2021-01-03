79aa99cd15aae5841591bde7080d9e8813d3dbd1
= <ggirlk2@gmail.com>
2021-01-03 01:12:51 +0100

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -18,8 +18,10 @@ def one_hot_encode(Y, classes):
     mx = np.zeros((classes, m))
     i = 0
     for n in Y:
-        if type(n) is not np.int64 or n < 0:
+        if type(n) is not np.int64:
             return None
+        if n < 0:
+            n *= -1
         mx[n:n+1, i:i+1] = 1
         i += 1
     return mx
