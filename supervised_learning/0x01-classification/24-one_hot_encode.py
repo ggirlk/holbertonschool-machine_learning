aed468feed087a707e10aee2fd03dc6d8ac48dda
= <ggirlk2@gmail.com>
2021-01-03 01:47:00 +0100

    one hot encode/decode~

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,13 +8,14 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ doc """
+    m = Y.shape[0]
     if type(Y) is not np.ndarray\
        or len(Y) == 0\
        or type(classes) is not int\
        or len(Y.shape) != 1\
+       or classes != m\
        or classes != Y.max()+1:
         return None
-    m = Y.shape[0]
     mx = np.zeros((classes, m))
     i = 0
     for n in Y:
