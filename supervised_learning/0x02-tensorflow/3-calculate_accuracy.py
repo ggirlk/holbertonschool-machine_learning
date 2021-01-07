0d444f5a0d19260afe2cb195d1d26ba20eda8a50
= <ggirlk2@gmail.com>
2021-01-07 23:19:56 +0100

    @_@

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """ doc """
     #acc = tf.compat.v1.metrics.accuracy(y, y_pred)
-    acc = tf.math.reduce_mean(y == y_pred)
+    acc = tf.math.reduce_sum(y == y_pred)/y.shape[0]
     return tf.cast(acc, tf.float32)
