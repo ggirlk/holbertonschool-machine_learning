cd2125c63a055a6016880a362f21e656d600ea2a
= <ggirlk2@gmail.com>
2021-01-07 23:40:12 +0100

    @_@

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def calculate_loss(y, y_pred):
     """ doc """
-    return tf.compat.v1.losses.mean_squared_error(
-        y, y_pred, weights=1.0, scope=None,
+    return tf.compat.v1.losses.softmax_cross_entropy(
+        y, y_pred, weights=1.0, label_smoothing=0, scope=None,
         loss_collection=tf.GraphKeys.LOSSES, reduction=Reduction.SUM_BY_NONZERO_WEIGHTS
     )
