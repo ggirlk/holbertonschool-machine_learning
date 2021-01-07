39e5a02c349c27af61b24e2346f00ae45a7c379e
= <ggirlk2@gmail.com>
2021-01-07 23:46:32 +0100

    @_@

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" doc """
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """ doc """
+    gr = tf.train.GradientDescentOptimizer(alpha)
+    return gr.compute_gradients(loss)
