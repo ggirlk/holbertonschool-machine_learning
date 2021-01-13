b76b21d2bcc2565fa5a4ce287dd73808f5da27b0
= <ggirlk2@gmail.com>
2021-01-13 21:16:18 +0100

    every day I'm shuffling

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -8,8 +8,6 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """ doc """
-    np.random.seed(0)
     x = np.random.permutation(X)
-    np.random.seed(0)
     y = np.random.permutation(Y)
     return x, y
