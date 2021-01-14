a0eca1f6e06475dff18934f600cf3906b1fe9971
= <ggirlk2@gmail.com>
2021-01-14 09:58:13 +0100

    momentum

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -9,13 +9,8 @@ import numpy as np
 # update_variables_momentum(0.01, 0.9, W, dW, dW_prev)
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """ doc """
-    m_avg = []
-    vt=0
-    for i in range(len(grad)):
-        vt = (vt*beta1 + grad[i]*(1-beta1))
-        # avg = vt/(1-beta1**(i+1))
-        m_avg.append(vt)
 
-    newgrad = np.array(m_avg)
-    newgrad = np.subtract(var, np.multiply(newgrad, alpha))
+    vt = np.add(v*beta1, grad*(1-beta1))
+
+    newgrad = np.subtract(var, np.multiply(vt, alpha))
     return newgrad, grad
