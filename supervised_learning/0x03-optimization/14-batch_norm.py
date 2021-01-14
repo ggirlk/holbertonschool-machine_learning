6f699967e6c8d7137779f411350a32f58e7c7c41
= <ggirlk2@gmail.com>
2021-01-14 22:20:36 +0100

    @_@

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -13,13 +13,15 @@ def create_batch_norm_layer(prev, n, activation):
             variance_scaling_initializer(mode="FAN_AVG"))
     layer = tf.layers.Dense(n, activation,
                             kernel_initializer=init)(prev)
-    output = tf.nn.batch_norm_with_global_normalization(
-                t = layer,
-                m = tf.nn.moments(layer, axes=[0]),
-                v = tf.nn.moments(layer, axes=[0]),
-                beta = tf.zeros_initializer(),
-                gamma = tf.ones_initializer(),
-                variance_epsilon = 1e-8,
-                scale_after_normalization = None)
-
-    return activation(output)
+    m, v = tf.nn.moments(layer, axes=[0])
+    beta = tf.Variable(tf.zeros([n]))
+    gamma = tf.Variable(tf.ones([n]))
+    epsilon = 1e-8,
+    scale = None
+    output = tf.nn.batch_norm_with_global_normalization(layer,
+                                                        m, v,
+                                                        beta,
+                                                        gamma,
+                                                        epsilon,
+                                                        scale)
+    return output
