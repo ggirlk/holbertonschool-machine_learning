16b48faaa0e56a7842d1c3e2eefa4fff37613694
= <ggirlk2@gmail.com>
2021-01-14 23:58:13 +0100

    @_@

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -4,9 +4,20 @@ training operation for a neural network
 in tensorflow using the gradient descent
 with momentum optimization algorithm
 """
-import tensorflow as tf
+import numpy as np
 
 
-def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+def update_variables_Adam(alpha, beta1, beta2,
+                          epsilon, var, grad, v, s, t):
     """ doc """
-    
+    st = np.add(s*beta2, (grad**2)*(1-beta2))
+    sc = st / (1-beta2**t)
+
+    vt = np.add(v*beta1, grad*(1-beta1))
+    vc = vt / (1-beta1**t)
+
+    newgrad = np.subtract(var,
+                          np.divide(np.multiply(vc, alpha),
+                                    (np.sqrt(sc)+epsilon)))
+
+    return newgrad, vt, st
