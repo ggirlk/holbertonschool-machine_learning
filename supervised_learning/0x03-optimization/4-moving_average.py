0e9eb5bb148b36eed02bd1ded63e26419089afb2
= <ggirlk2@gmail.com>
2021-01-14 04:57:03 +0100

    emwa

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -3,22 +3,15 @@
 calculates the weighted
 moving average of a data set
 """
+import numpy as np
 
 
 def def moving_average(data, beta):
     """ doc """
     m_avg = []
+    vt = 0
     for i in range(len(data)):
-        if i == 0:
-            avg = data[i]
-        else:
-            d = 0
-            coef = 1
-            for j in range(i):
-                d += data[j]*beta
-                coef += beta
-            # print(data[j], data[i])
-            # print("-----", coef)
-            avg = (d + data[i])/(coef)
+        vt = (vt*beta + data[i]*(1-beta))
+        avg = vt/(1-beta**(i+1))
         m_avg.append(avg)
     return m_avg
