0bd481768f32354318fee3e72be4ecfdba0da896
= <ggirlk2@gmail.com>
2021-01-14 11:35:13 +0100

    @_@

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -9,5 +9,5 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ doc """
-    new = tf.train.inverse_time_decay(alpha, decay_rate, global_step, decay_step)
+    new = tf.train.inverse_time_decay(alpha, global_step, decay_step, decay_rate)
     return new
