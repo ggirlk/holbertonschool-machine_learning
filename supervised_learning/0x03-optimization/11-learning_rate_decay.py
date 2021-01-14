5f276af31caf75e81608b0a6dbf0d8bc2345669b
= <ggirlk2@gmail.com>
2021-01-15 00:35:10 +0100

    @_@

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -4,9 +4,9 @@ training operation for a neural network
 in tensorflow using the gradient descent
 with momentum optimization algorithm
 """
-import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ doc """
-    
+
+    return alpha/(1+(decay_rate*(global_step)))
