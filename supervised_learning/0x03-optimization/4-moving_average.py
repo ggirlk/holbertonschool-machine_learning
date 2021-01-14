eccf382e2e52478eda3acbb4b9e7dce47d8d2e93
= <ggirlk2@gmail.com>
2021-01-14 02:22:30 +0100

    mwa

---
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+calculates the weighted
+moving average of a data set
+"""
+import numpy as np
+
+
+def def moving_average(data, beta):
+    """ doc """
+    m_avg = []
+    for i in range(len(data)):
+        if i == 0:
+            avg = data[i]
+        else:
+            d = 0
+            coef = 1
+            for j in range(i):
+                d += data[j]*beta
+                coef += beta
+            # print(data[j], data[i])
+            # print("-----", coef)
+            avg = (d + data[i])/(coef)
+        m_avg.append(avg)
+    return m_avg
