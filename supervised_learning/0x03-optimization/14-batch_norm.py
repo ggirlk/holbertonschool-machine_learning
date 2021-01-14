ab04a7f2637bdf5ae247477615b35926a84521c5
= <ggirlk2@gmail.com>
2021-01-14 11:56:04 +0100

    @_@

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -9,5 +9,8 @@ import tensorflow as tf
 
 def create_batch_norm_layer(prev, n, activation):
     """ doc """
-    layer = tf.layers.Dense(n, activation)(prev)
+    init = (tf.contrib.layers.
+            variance_scaling_initializer(mode="FAN_AVG"))
+    layer = tf.layers.Dense(n, activation,
+                            kernel_initializer=init)(prev)
     return layer
