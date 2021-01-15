eea7d91db232fe2ff30e4e3db6bcad5d06c1dfff
= <ggirlk2@gmail.com>
2021-01-15 01:44:38 +0100

    znorm

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -4,9 +4,11 @@ training operation for a neural network
 in tensorflow using the gradient descent
 with momentum optimization algorithm
 """
-import tensorflow as tf
 
 
 def batch_norm(Z, gamma, beta, epsilon):
     """ doc """
-    
+    µ = Z.mean()
+    var = ((Z-µ)**2).mean()
+    znorm = (Z-µ)/(np.sqrt(var+epsilon))
+    return gamma*znorm + beta
