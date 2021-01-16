af5f13c217814a9ba3fa669ce32bfb6d45656106
= <ggirlk2@gmail.com>
2021-01-16 12:10:25 +0100

    🤯

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -47,8 +47,6 @@ def train(X_train, Y_train,
                       + "\n\tValidation Accuracy: {}".format(accuracy_valid))
             # Training data
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-            if i == iterations:
-                break
         # Training session Saver
         trainSaver = tf.train.Saver()
         return trainSaver.save(sess, save_path)
