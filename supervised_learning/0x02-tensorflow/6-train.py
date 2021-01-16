f3c9a7df7b539992b84148728ae288e7899b863d
= <ggirlk2@gmail.com>
2021-01-16 11:52:29 +0100

    🤯

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -36,10 +36,16 @@ def train(X_train, Y_train,
             # Displaying training result on current iteration
             if (i % 100 == 0 or i == iterations):
                 # Calculating costs && accuracies on current iteration 
-                cost_train = loss.eval({x : X_train, y : Y_train}, sess)
-                accuracy_train = acc.eval({x : X_train, y : Y_train}, sess)
-                cost_valid = loss.eval({x : X_valid, y : Y_valid}, sess)
-                accuracy_valid = acc.eval({x : X_valid, y : Y_valid}, sess)
+                cost_train, accuracy_train = sess.run((loss, acc),
+                                                feed_dict={x: X_train,
+                                                           y: Y_train})
+                cost_valid, accuracy_valid = sess.run((loss, acc),
+                                                feed_dict={x: X_train,
+                                                           y: Y_train})
+                """cost_train = loss.eval({x : X_train, y : Y_train})
+                accuracy_train = acc.eval({x : X_train, y : Y_train})
+                cost_valid = loss.eval({x : X_valid, y : Y_valid})
+                accuracy_valid = acc.eval({x : X_valid, y : Y_valid})"""
                 print("After {} iterations:".format(i)
                       +"\n\tTraining Cost: {}".format(cost_train)
                       + "\n\tTraining Accuracy: {}".format(accuracy_train)
