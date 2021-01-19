867b5bd429221ea1a34e5de9a5528a7f9ebe59b9
= <ggirlk2@gmail.com>
2021-01-19 07:08:32 +0100

    matrix

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -8,10 +8,14 @@ def create_confusion_matrix(labels, logits):
     m = labels.shape[0] # m 
     k = labels.shape[1] # Number of classes 
     result = np.zeros((k, k))
-    pred =  np.argmax(logits, axis=1)
-    true = np.argmax(labels, axis=1)
+    y_pred = np.argmax(logits, axis=1)
+    y_true = np.argmax(labels, axis=1)
+    ind = np.logical_and(y_pred < k, y_true < k)
+    y_pred = y_pred[ind]
+    y_true = y_true[ind]
+    sample_weight = np.ones(m, dtype=np.int64)
+    sample_weight = sample_weight[ind]
     for i in range(m):
-        for j in range(k):
-            a, b = true[i], pred[i]
-            result[a][b] += 1
+        a, b = y_true[i], y_pred[i]
+        result[a][b] += sample_weight[i]
     return (result)
