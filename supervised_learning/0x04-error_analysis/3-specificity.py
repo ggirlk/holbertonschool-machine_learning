3bac80762fb92292989e266680cf370e26b3bfb5
= <ggirlk2@gmail.com>
2021-01-20 12:42:24 +0100

    specificity

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -8,5 +8,5 @@ def specificity(confusion):
     true_pos = np.diag(confusion)
     false_pos = np.sum(confusion, axis=0) - true_pos
     false_neg = np.sum(confusion, axis=1) - true_pos
-    true_neg =  np.sum(confusion) - false_neg - true_pos
-    return (np.round(true_neg / (true_neg + false_pos), 8))
+    true_neg = np.sum(confusion) - false_neg - true_pos
+    return (true_neg / (true_neg + false_pos))
