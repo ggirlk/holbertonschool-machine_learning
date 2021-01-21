0a95fdd4f2db1b31213ab24b711d7e56b1e1be53
= <ggirlk2@gmail.com>
2021-01-21 06:41:42 +0100

    specificity

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -5,12 +5,8 @@ import numpy as np
 
 def specificity(confusion):
     """ doc """
-    m = confusion.shape[0]
-    r = []
-    for i in range(m):
-        tp = confusion[i][i]
-        fn = confusion.T[i].sum()-tp
-        tn = confusion.sum() - fn - tp
-        allnegatives = confusion.sum() - confusion[i].sum()
-        r.append(np.round(tn/allnegatives, 8))
-    return np.array(r)
+    true_pos = np.diag(confusion)
+    false_pos = np.sum(confusion, axis=0) - true_pos
+    false_neg = np.sum(confusion, axis=1) - true_pos
+    true_neg = np.sum(confusion) - false_neg - true_pos - false_pos
+    return (true_neg / (true_neg + false_pos))
