c1007b25f219003b01281a2fd55f5601af5e119d
= <ggirlk2@gmail.com>
2021-01-22 15:36:18 +0100

    l2

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -7,6 +7,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """ doc """
     sw = 0
     for i in range(1, L):
-        sw += (weights['W'+str(i)]**2).sum()
-    l2 = (lambtha*sw**0.5)/(2*m)
+        sw += np.linalg.norm(weights['W'+str(i)])
+    l2 = (lambtha*sw)/(2*m)
     return (cost+l2)
