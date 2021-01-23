e097552b03b50a2d83df73c0967d6287620205c8
= <ggirlk2@gmail.com>
2021-01-23 20:20:38 +0100

    fprop

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -9,7 +9,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     init = (tf.contrib.layers.
             variance_scaling_initializer(mode="FAN_AVG"))
     freg = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(n,activation, name='layer',
+    layer = tf.layers.Dense(n, activation, name='layer',
                             kernel_initializer=init,
                             kernel_regularizer=freg)(prev)
     return (layer)
---
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" doc """
+import tensorflow as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """ doc """
+    cache = {}
+    cache["A0"] = X
+    for i in range(1, L):
+        W = tf.cast(weights["W"+str(i)], dtype="float32")
+        b = tf.cast(weights["b"+str(i)], dtype="float32")
+        dropped = tf.nn.dropout(cache['A'+str(i-1)], keep_prob)
+        dense = tf.matmul(dropped,W)+b
+        cache['A'+str(i)] = tf.nn.softmax(dense)
+    i += 1
+    W = tf.cast(weights["W"+str(i)], dtype="float32")
+    b = tf.cast(weights["b"+str(i)], dtype="float32")
+    dropped = tf.layers.Dropout(cache['A'+str(i-1)], keep_prob)
+    dense = tf.matmul(dropped, W)+b
+    cache['A'+str(i)] = tf.nn.tanh(dense)
+    return cache
