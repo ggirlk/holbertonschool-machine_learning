705fa8e240f9b13a2c8b679d323dc1a41697226d
= <ggirlk2@gmail.com>
2021-01-23 19:15:51 +0100

    layer

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -8,7 +8,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
 
     init = (tf.contrib.layers.
             variance_scaling_initializer(mode="FAN_AVG"))
-    layer = tf.layers.Dense(n, name='layer',
-                            kernel_regularizer=lambtha,
-                            kernel_initializer=init)(prev)
-    return activation(layer)
+    freg = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(n,activation, name='layer',
+                            kernel_initializer=init,
+                            kernel_regularizer=freg)(prev)
+    return (layer)
