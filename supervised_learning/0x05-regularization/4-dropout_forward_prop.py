202717e8451c897223281bbb20c9f8626e60094c
= <ggirlk2@gmail.com>
2021-01-24 10:38:21 +0100

    dropout from scratch

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -6,20 +6,26 @@ import numpy as np
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """ doc """
+    def softmax(z):
+        """ softmax function """
+        return np.exp(z)/(np.sum(np.exp(z), axis=0))
+    def tanh(z):
+        """ tanh function """
+        return np.tanh(z)
+    np.random.seed(1)
     cache = {}
     cache["A0"] = X
-    for i in range(1, L):
-        W = tf.cast(weights["W"+str(i)], dtype="float32")
-        b = tf.cast(weights["b"+str(i)], dtype="float32")
-        dropped = tf.nn.dropout(cache['A'+str(i-1)], keep_prob)
-        print(W.shape, b.shape, dropped.shape)
-        dense = np.dot(dropped, tf.transpose(W)) + b
-        cache['A'+str(i)] = tf.nn.softmax(dense)
-        cache['D'+str(i)] = dense
-    i += 1
-    W = tf.cast(weights["W"+str(i)], dtype="float32")
-    b = tf.cast(weights["b"+str(i)], dtype="float32")
-    dropped = tf.layers.Dropout(cache['A'+str(i-1)], keep_prob)
-    dense = np.dot(dropped, tf.transpose(W))+b
-    cache['A'+str(i)] = tf.nn.tanh(dense)
+    for i in range(1, L+1):
+        W = weights["W"+str(i)]
+        b = weights["b"+str(i)]
+        A = cache['A'+str(i-1)]
+        Z = np.dot(W, A) + b
+        if i != L:
+            cache['A'+str(i)] = tanh(Z)
+            A = cache['A'+str(i)]
+            cache["D"+str(i)] = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
+            cache["D"+str(i)] = np.where(cache["D"+str(i)] < keep_prob, 0, 1)
+            cache['A'+str(i)] = np.multiply(A, cache["D"+str(i)]) / keep_prob
+        else:
+            cache['A'+str(i)] = softmax(Z)
     return cache
