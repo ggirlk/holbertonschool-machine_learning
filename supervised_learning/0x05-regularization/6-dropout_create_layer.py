84634fd1d7b5833c57b7ea7e0efcad640ee2fad0
= <ggirlk2@gmail.com>
2021-01-24 15:18:13 +0100

    layer

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -5,9 +5,10 @@ import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """ doc """
-    dropped = tf.nn.dropout(prev, keep_prob=keep_prob)
+    dropped = tf.layers.Dropout(rate=keep_prob)
     init = (tf.contrib.layers.
             variance_scaling_initializer(mode="FAN_AVG"))
-    layer = tf.layers.Dense(n, name='layer',
-                            kernel_initializer=init)(dropped)
-    return activation(layer)
+    layer = tf.layers.Dense(n, activation, name='layer',
+                            kernel_initializer=init,
+                            activity_regularizer=dropped)(prev)
+    return (layer)
