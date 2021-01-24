7bef35e9585e683c67da56f3b50bdc7e47baaf87
= <ggirlk2@gmail.com>
2021-01-24 14:47:13 +0100

    layer

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """ doc """
 import numpy as np
-import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """ doc """
-    dropped = tf.nn.dropout(prev, keep_prob=keep_prob)
-    init = (tf.contrib.layers.
-            variance_scaling_initializer(mode="FAN_AVG"))
-    layer = tf.layers.Dense(n, activation, name='layer',
-                            kernel_initializer=init)(dropped)
+    A = activation(prev)
+    d = np.random.rand(A.shape[1], n) < keep_prob
+    d = np.where(d < keep_prob, 0, 1)
+    layer = np.multiply(A, d) / keep_prob
     return layer
