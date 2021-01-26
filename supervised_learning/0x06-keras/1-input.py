a1711b7a56475f319402faf3141296e6b72d01f0
= <ggirlk2@gmail.com>
2021-01-26 17:41:02 +0100

    Keras Input

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -6,16 +6,15 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     inputs = K.Input(shape=(nx,))
-    init = K.initializers.VarianceScaling(mode="fan_avg")
-    freg = K.layers.ActivityRegularization(l2=lambtha)
-    dropped = K.layers.Dropout(rate=keep_prob)
-    x = K.layers.Dense(layers[0], activation=activations[0],
-                       kernel_initializer=init,
-                       kernel_regularizer=freg)(inputs)
-    for i in range(1, len(layers)):
+    x = inputs
+    for i in range(0, len(layers)):
+        init = K.initializers.VarianceScaling(mode="fan_avg")
+        freg = K.regularizers.l2(lambtha)
+        dropped = K.layers.Dropout(rate=1-keep_prob)
         x = K.layers.Dense(layers[i], activation=activations[i],
                            kernel_initializer=init,
-                           kernel_regularizer=freg)(x)
+                           kernel_regularizer=freg,
+                           activity_regularizer=dropped)(x)
     model = tf.keras.Model(inputs=inputs, outputs=x)
-    model.Dropout = dropped
+    # model.Dropout = dropped
     return (model)
