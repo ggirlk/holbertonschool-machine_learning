af0796c1532a42ecd184e1e67fb9ade7fbce89f8
= <ggirlk2@gmail.com>
2021-01-26 12:38:24 +0100

    Keras Sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -6,16 +6,11 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     model = K.Sequential()
-    freg = K.regularizers.l2(l=lambtha)
     for i in range(len(layers)):
-        if i == 0:
-            layer = K.layers.Dense(layers[i],  input_dim=nx,
-                                     activation=activations[i],
-                                     kernel_regularizer=freg)
-        else:
-            layer = K.layers.Dense(layers[i],
-                                     activation=activations[i],
-                                     kernel_regularizer=freg)
+        freg = K.regularizers.l2(l=lambtha)
+        layer = K.layers.Dense(layers[i],  input_dim=nx,
+                               activation=activations[i],
+                               kernel_regularizer=freg)
         model.add(layer)
         if i != len(layers)-1:
             dropped = K.layers.Dropout(rate=keep_prob)
