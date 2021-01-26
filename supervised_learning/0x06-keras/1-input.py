248cd196353b4dce7e4382ad0421d44494755b02
= <ggirlk2@gmail.com>
2021-01-26 14:58:30 +0100

    Keras Sequential

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -7,12 +7,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     model = K.Sequential()
     for i in range(len(layers)):
+        init = K.initializers.VarianceScaling(mode="fan_avg")
         freg = K.layers.ActivityRegularization(l2=lambtha)
-        layer = K.layers.Dense(layers[i], input_dim=nx,
+        layer = K.layers.Dense(layers[i],
                                activation=activations[i],
+                               kernel_initializer=init,
                                kernel_regularizer=freg)
         model.add(layer)
         if i != len(layers)-1:
             dropped = K.layers.Dropout(rate=keep_prob)
             model.add(dropped)
+    model.build((None, nx))
     return (model)
