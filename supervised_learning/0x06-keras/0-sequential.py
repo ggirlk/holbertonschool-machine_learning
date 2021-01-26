c7583538c65075d5c44d17164164a8219c32ed19
= <ggirlk2@gmail.com>
2021-01-26 14:05:40 +0100

    Keras Sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 """ doc """
 import tensorflow.keras as K
+import tensorflow as tf
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     model = K.Sequential()
-    freg = K.layers.ActivityRegularization(l2=lambtha)
-    layer = K.layers.Dense(layers[0], input_dim=nx,
-                           activation=activations[0],
-                           kernel_regularizer=freg)
-    model.add(layer)
-    dropped = K.layers.Dropout(rate=keep_prob)
-    model.add(dropped)
-    for i in range(1, len(layers)):
+    for i in range(len(layers)):
+        init = (tf.contrib.layers.
+            variance_scaling_initializer(mode="FAN_AVG"))
         freg = K.layers.ActivityRegularization(l2=lambtha)
-        layer = K.layers.Dense(layers[i],
+        layer = K.layers.Dense(layers[i], input_dim=nx,
                                activation=activations[i],
+                               kernel_initializer=init,
                                kernel_regularizer=freg)
         model.add(layer)
-        #if i != len(layers)-1:
-        dropped = K.layers.Dropout(rate=keep_prob)
-        model.add(dropped)
+        if i != len(layers)-1:
+            dropped = K.layers.Dropout(rate=keep_prob)
+            model.add(dropped)
     return (model)
