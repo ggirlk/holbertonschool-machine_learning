be972b15ca72759f3b43d24cc4c7187f7b59981c
= <ggirlk2@gmail.com>
2021-01-26 19:22:14 +0100

    Keras Input

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -7,12 +7,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     inputs = K.Input(shape=(nx,))
     freg = K.regularizers.l2(lambtha)
-    x = inputs
-    for i in range(0, len(layers)):
+    x = K.layers.Dense(layers[0], activation=activations[0],
+                           kernel_regularizer=freg)(inputs)
+    for i in range(1, len(layers)):
+        x = K.layers.Dropout(rate=1-keep_prob)(x)
         x = K.layers.Dense(layers[i], activation=activations[i],
                            kernel_regularizer=freg)(x)
-        if i != len(layers)-1:
-            x = K.layers.Dropout(rate=1-keep_prob)(x)
-        
+
     model = tf.keras.Model(inputs=inputs, outputs=x)
     return (model)
