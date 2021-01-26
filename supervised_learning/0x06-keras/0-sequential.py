19edb9cbddc6934d18b00765f6448cdf1530c03f
= <ggirlk2@gmail.com>
2021-01-26 14:40:55 +0100

    Keras Sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -7,7 +7,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """ doc """
     model = K.Sequential()
     for i in range(len(layers)):
-        init = K.initializers.VarianceScaling(mode="FAN_AVG")
+        init = K.initializers.VarianceScaling(mode="fan_avg")
         freg = K.layers.ActivityRegularization(l2=lambtha)
         layer = K.layers.Dense(layers[i], input_dim=nx,
                                activation=activations[i],
