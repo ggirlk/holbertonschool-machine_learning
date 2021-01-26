52bb6c050c381f4be52f478998ec736fe2c58e77
= <ggirlk2@gmail.com>
2021-01-26 21:56:11 +0100

    Keras training

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -6,10 +6,6 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """ doc """
-    if verbose == True:
-        verbose = 1
-    else:
-        verbose = 0
     return network.fit(data, labels,
                        batch_size=batch_size,
                        epochs=epochs,
