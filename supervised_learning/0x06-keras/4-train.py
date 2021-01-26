5693592fdc17039c8b2ac3ff90d70bddd5136113
= <ggirlk2@gmail.com>
2021-01-26 21:34:49 +0100

    Keras training

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """ doc """
 import tensorflow.keras as K
-import tensorflow as tf
 
 
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """ doc """
-    tf.set_random_seed(0)
     return network.fit(data, labels,
                        batch_size=batch_size,
                        epochs=epochs,
