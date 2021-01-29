e3b1ca1186355fa5dfbb558ce2274565be8d8e32
= <ggirlk2@gmail.com>
2021-01-29 19:09:51 +0100

    lr decay

---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" doc """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False,
+                alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    """ doc """
+    callbacks = None
+    if validation_data:
+        callbacks = [K.callbacks.EarlyStopping(monitor="loss",
+                                               patience=patience,
+                                               mode="auto")]
+        
+        def scheduler(epoch):
+            e = 2.7182818285
+            if epoch < 10:
+                return alpha
+            else:
+                return alpha * e**(-0.1)
+        callbacks.append(K.callbacks.LearningRateScheduler(scheduler, 1))
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       callbacks=callbacks,
+                       validation_data=validation_data,
+                       shuffle=shuffle)
