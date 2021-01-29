7b09ffae4aa7b2b2a37cc449b5592b0efa228210
= <ggirlk2@gmail.com>
2021-01-29 20:42:49 +0100

    Save Only the Best

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -21,11 +21,7 @@ def train_model(network, data, labels, batch_size,
             return alpha/(1+(decay_rate*(epoch)))
         callbacks.append(K.callbacks.LearningRateScheduler(scheduler, 1))
     if save_best:
-        callbacks.append(K.callbacks.ModelCheckpoint(filepath,
-                                                     monitor='loss',
-                                                     save_best_only=save_best,
-                                                     verbose=0,
-                                                     mode='auto'))
+        callbacks.append(K.callbacks.ModelCheckpoint(filepath))
 
     return network.fit(data, labels,
                        batch_size=batch_size,
