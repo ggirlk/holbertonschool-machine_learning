ccd74f879f520a3e58b28b8054343f06ac4b1e78
= <ggirlk2@gmail.com>
2021-01-29 20:25:32 +0100

    Save Only the Best

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" doc """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, save_best=False, filepath=None,
+                verbose=True, shuffle=False):
+    """ doc """
+    callbacks = None
+    if validation_data:
+        callbacks = [K.callbacks.EarlyStopping(monitor="loss",
+                                               patience=patience,
+                                               mode="auto")]
+
+        def scheduler(epoch):
+            """ new learning """
+            return alpha/(1+(decay_rate*(epoch)))
+        callbacks.append(K.callbacks.LearningRateScheduler(scheduler, 1))
+        callbacks.append(K.callbacks.ModelCheckpoint(filepath,
+                                                     monitor='val_loss',
+                                                     save_best_only=save_best,
+                                                     mode='auto'))
+
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       callbacks=callbacks,
+                       validation_data=validation_data,
+                       shuffle=shuffle)
