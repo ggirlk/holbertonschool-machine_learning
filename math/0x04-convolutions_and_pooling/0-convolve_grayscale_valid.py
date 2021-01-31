3e7e8b55b03cdec1070a78eea41a30c07fbcf430
= <ggirlk2@gmail.com>
2021-01-31 23:00:33 +0100

    Valid Convolution

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,10 +9,10 @@ def convolve_grayscale_valid(images, kernel):
     kw = kernel.shape[1]
     output = np.zeros_like(images)
     for i in range(images.shape[1]):
-        if i < images.shape[1] - kh:
+        if i <= images.shape[1] - kh:
             image = images[i]
             for j in range(images.shape[2]):
-                if j < images.shape[2] - kw:
+                if j <= images.shape[2] - kw:
                     output[:, i, j] = np.tensordot(images[:,
                                                           i:i+kh,
                                                           j:j+kw],
