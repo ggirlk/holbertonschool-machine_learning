0d18bdfad64c95172dd5cb56a94a4f1d46b6c0d7
= <ggirlk2@gmail.com>
2021-02-02 15:55:31 +0100

    Same Convolution

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -13,10 +13,10 @@ def convolve_grayscale_same(images, kernel):
                        imgh,
                        imgw))
     for i in range(images.shape[1]):
-        if i <= images.shape[1] - kh:
+        if i < images.shape[1] - kh:
             image = images[i]
             for j in range(images.shape[2]):
-                if j <= images.shape[2] - kw:
+                if j < images.shape[2] - kw:
                     output[:, i, j] = np.tensordot(images[:,
                                                           i:i+kh,
                                                           j:j+kw],
