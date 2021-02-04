3e003b933d67b4754203eddb6b8e23f985ebdd22
= <ggirlk2@gmail.com>
2021-02-04 04:50:52 +0100

    Same Convolution

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,24 +5,15 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """ doc """
-    kh = kernel.shape[0]
-    kw = kernel.shape[1]
-    imgh = images.shape[1]
-    imgw = images.shape[2]
-    output = np.zeros((images.shape[0],
-                       imgh,
-                       imgw))
-    padded = np.zeros((images.shape[0],
-                       imgh + kh - 1 ,
-                       imgw + kw - 1))
-    padded[:, kh-1:-kh+1, kw-1:-kw+1] = images
-    for i in range(images.shape[1]):
-        if i < images.shape[1] - kh:
-            image = images[i]
-            for j in range(images.shape[2]):
-                if j < images.shape[2] - kw:
-                    output[:, i, j] = np.tensordot(padded[:,
-                                                          i:i+kh,
-                                                          j:j+kw],
-                                                   kernel)
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    imghp, imgwp = kh//2, kw//2
+    output = np.zeros((m, h, w))
+    new = np.pad(images, ((0, 0), (imghp, imghp), (imgwp, imgwp)), 'constant')
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.tensordot(new[:,
+                                           i:i+kh,
+                                           j:j+kw],
+                                           kernel)
     return output
