0c3a0a0564ec91e2cb8775edefe4154129514791
= <ggirlk2@gmail.com>
2021-02-05 04:32:12 +0100

    Strided Convolution

---
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" doc """
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """ doc """
+    m, imgh, imgw = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+    if padding == 'same':
+        imghp, imgwp = kh//2, kw//2
+    if padding == 'valid':
+        imghp, imgwp = 0, 0
+    if type(padding) == tuple:
+        imghp, imgwp = padding
+    imgh, imgw = (imgh-kh+2*imghp)//sh + 1, (imgw-kw+2*imgwp)//sw + 1
+    output = np.zeros((m, imgh, imgw))
+    new = np.pad(images, ((0, 0), (imghp, imghp), (imgwp, imgwp)), 'constant')
+    a, b = 0, 0
+    for i in range(imgh):
+        for j in range(imgw):
+            output[:, i, j] = np.tensordot(new[:,
+                                           i*sh:i*sh+kh,
+                                           j*sw:j*sw+kw],
+                                           kernel)
+    return output
