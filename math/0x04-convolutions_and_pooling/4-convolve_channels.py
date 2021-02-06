fffb0eabc4e49b858760916910440759091adc64
= <ggirlk2@gmail.com>
2021-02-06 19:28:01 +0100

    Convolution with Channels

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -6,7 +6,7 @@ import numpy as np
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """ doc """
     m, imgh, imgw, c = images.shape
-    kh, kw, kc, knc = kernels.shape
+    kh, kw, kc = kernel.shape
     sh, sw = stride
     imghp, imgwp = 0, 0
     if padding == 'same':
@@ -16,16 +16,13 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if type(padding) == tuple:
         imghp, imgwp = padding
     imgh, imgw = (imgh-kh+2*imghp)//sh + 1, (imgw-kw+2*imgwp)//sw + 1
-    output = np.zeros((m, imgh, imgw, c))
+    output = np.zeros((m, imgh, imgw))
     new = np.pad(images, ((0, 0), (imghp, imghp),
                           (imgwp, imgwp), (0, 0)), 'constant')
-    for k in range(knc):
-        for i in range(imgh):
-            for j in range(imgw):
-                output[:, i, j, k] = np.sum(new[:,
-                                                i*sh:i*sh+kh,
-                                                j*sw:j*sw+kw, :]
-                                            * kernels[..., k],
-                                            axis=(1, 2, 3))
+    for i in range(imgh):
+        for j in range(imgw):
+            output[:, i, j] = np.sum(new[:,
+                                         i*sh:i*sh+kh,
+                                         j*sw:j*sw+kw, :]*kernel,
+                                     axis=(1, 2, 3))
     return output
-
