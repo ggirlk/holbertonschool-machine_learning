c7cd9b7e033cbe2dbb39022e2baa8c8fad7acefe
= <ggirlk2@gmail.com>
2021-02-06 05:38:00 +0100

    Convolution with Channels

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -6,13 +6,13 @@ import numpy as np
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """ doc """
     m, imgh, imgw, c = images.shape
-    kh, kw = kernel.shape
+    kh, kw, kc = kernel.shape
     sh, sw = stride
+    imghp, imgwp = 0, 0
     if padding == 'same':
         imghp = (((imgh - 1) * sh + kh - imgh) // 2) + 1
         imgwp = (((imgw - 1) * sw + kw - imgw) // 2) + 1
-    if padding == 'valid':
-        imghp, imgwp = 0, 0
+        
     if type(padding) == tuple:
         imghp, imgwp = padding
     imgh, imgw = (imgh-kh+2*imghp)//sh + 1, (imgw-kw+2*imgwp)//sw + 1
