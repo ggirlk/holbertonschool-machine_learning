0bdb57b434217879b781903a3f811c1eece8f2d9
= <ggirlk2@gmail.com>
2021-02-07 23:54:19 +0100

    leNet5 TensorFlow Kerras

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -6,19 +6,19 @@ import tensorflow.keras as K
 def lenet5(X):
     """ doc """
     layer = K.layers.Conv2D(6, 5, padding='same',
-                          activation='relu',
-                          kernel_initializer='he_normal')(X)
+                            activation='relu',
+                            kernel_initializer='he_normal')(X)
     layer = K.layers.MaxPooling2D(2, 2)(layer)
     layer = K.layers.Conv2D(16, 5, padding='valid', activation='relu',
-                          kernel_initializer='he_normal')(layer)
+                            kernel_initializer='he_normal')(layer)
     layer = K.layers.MaxPooling2D(2, 2)(layer)
     layer = K.layers.Flatten()(layer)
     layer = K.layers.Dense(120, activation='relu',
-                         kernel_initializer='he_normal')(layer)
+                           kernel_initializer='he_normal')(layer)
     layer = K.layers.Dense(84, activation='relu',
-                         kernel_initializer='he_normal')(layer)
+                           kernel_initializer='he_normal')(layer)
     layer = K.layers.Dense(10, activation='softmax',
-                         kernel_initializer='he_normal')(layer)
+                           kernel_initializer='he_normal')(layer)
     model = K.Model(X, layer)
     model.compile('Adam', metrics=['accuracy'],
                   loss='categorical_crossentropy')
