a5b0298315cafc7d5f61373e24eed510a0e98e40
= <ggirlk2@gmail.com>
2021-02-11 00:59:12 +0100

    Identity Block

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -12,15 +12,21 @@ def identity_block(A_prev, filters):
     Dense = K.layers.Dense
     Concatenate = K.layers.Concatenate
     BatchNorm = K.layers.BatchNormalization
-    relu = K.layers.ReLU
+    act = K.layers.Activation
     add = K.layers.Add
     F11, F3, F12 = filters
-    layer1x1 = Conv2D(F11, 1, 1, padding='same',  activation='relu')(A_prev)
+    layer1x1 = Conv2D(F11, 1, 1, padding='same')(A_prev)
     layer1x1 = BatchNorm()(layer1x1)
-    layer3x3 = Conv2D(F3, 1, 1, padding='same',  activation='relu')(layer1x1)
+    layer1x1 = act('relu')(layer1x1)
+
+    layer3x3 = Conv2D(F3, 1, 1, padding='same')(layer1x1)
     layer3x3 = BatchNorm()(layer3x3)
+    layer3x3 = act('relu')(layer3x3)
+
     layer1x1 = Conv2D(F12, 4, 1, padding='same')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)
+
     layer_out = add()([layer1x1, A_prev])
-    #layer_out = relu(layer_out)
+    layer_out = act('relu')(layer_out)
+
     return layer_out
