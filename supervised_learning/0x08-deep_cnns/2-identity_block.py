da6d4b00468bb099858439317ab0f745ac45ee6e
= <ggirlk2@gmail.com>
2021-02-11 01:24:39 +0100

    Identity Block

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -7,22 +7,21 @@ def identity_block(A_prev, filters):
     """ doc """
     Conv2D = K.layers.Conv2D
     BatchNorm = K.layers.BatchNormalization
-    act = K.layers.Activation
-    add = K.layers.Add
+    Activation = K.layers.Activation
+    Add = K.layers.Add
     F11, F3, F12 = filters
-    layer1x1 = Conv2D(F11, 1, 1, padding='same')(A_prev)
+    layer1x1 = Conv2D(F11, 1, padding='same')(A_prev)
     layer1x1 = BatchNorm()(layer1x1)
-    layer1x1 = act('relu')(layer1x1)
+    layer1x1 = Activation('relu')(layer1x1)
 
-    layer3x3 = Conv2D(F3, 3, 1, padding='same')(layer1x1)
+    layer3x3 = Conv2D(F3, 3, padding='same')(layer1x1)
     layer3x3 = BatchNorm()(layer3x3)
-    layer3x3 = act('relu')(layer3x3)
+    layer3x3 = Activation('relu')(layer3x3)
 
-    layer1x1 = Conv2D(F12, 1, 1, padding='same')(layer3x3)
+    layer1x1 = Conv2D(F12, 1, padding='same')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)
 
-    layer_out = add()([layer1x1, A_prev])
-    layer_out = act('relu')(layer_out)
+    layer_out = Add()([layer1x1, A_prev])
+    layer_out = Activation('relu')(layer_out)
 
     return layer_out
-
