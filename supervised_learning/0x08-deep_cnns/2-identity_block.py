502150a8caa8fc3c054cd1d122a6bf57024cbc95
= <ggirlk2@gmail.com>
2021-02-11 11:37:50 +0100

    -_-

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -10,7 +10,7 @@ def identity_block(A_prev, filters):
     Activation = K.layers.Activation
     Add = K.layers.Add
     F11, F3, F12 = filters
-    layer1x1 = Conv2D(F11, 1, padding='same',
+    layer1x1 = Conv2D(F11, 1,
                       kernel_initializer='he_normal')(A_prev)
     layer1x1 = BatchNorm()(layer1x1)
     layer1x1 = Activation('relu')(layer1x1)
@@ -20,7 +20,7 @@ def identity_block(A_prev, filters):
     layer3x3 = BatchNorm()(layer3x3)
     layer3x3 = Activation('relu')(layer3x3)
 
-    layer1x1 = Conv2D(F12, 1, padding='same',
+    layer1x1 = Conv2D(F12, 1,
                       kernel_initializer='he_normal')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)
 
