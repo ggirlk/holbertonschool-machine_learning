93d0af60e40b88cc07d76660d4c5dbfc0f0deb0b
= <ggirlk2@gmail.com>
2021-02-11 03:34:01 +0100

    Projection Block

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -10,9 +10,6 @@ def projection_block(A_prev, filters, s=2):
     Activation = K.layers.Activation
     Add = K.layers.Add
     F11, F3, F12 = filters
-    layer1x1_s = Conv2D(F12, 1, s,padding='same',
-                        kernel_initializer='he_normal')(A_prev)
-    layer1x1_s = BatchNorm()(layer1x1_s)  #
 
     layer1x1 = Conv2D(F11, 1, s,padding='same',
                       kernel_initializer='he_normal')(A_prev)
@@ -27,6 +24,10 @@ def projection_block(A_prev, filters, s=2):
     layer1x1 = Conv2D(F12, 1,padding='same',
                       kernel_initializer='he_normal')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)  #
+
+    layer1x1_s = Conv2D(F12, 1, s,padding='same',
+                        kernel_initializer='he_normal')(A_prev)
+    layer1x1_s = BatchNorm()(layer1x1_s)  #
 
     layer_out = Add()([layer1x1, layer1x1_s])
     layer_out = Activation('relu')(layer_out)
