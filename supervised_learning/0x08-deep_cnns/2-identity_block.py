6ef08b2264e75c81f32139d16a22438289b48cab
= <ggirlk2@gmail.com>
2021-02-11 01:08:21 +0100

    Identity Block

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -14,14 +14,15 @@ def identity_block(A_prev, filters):
     layer1x1 = BatchNorm()(layer1x1)
     layer1x1 = act('relu')(layer1x1)
 
-    layer3x3 = Conv2D(F3, 1, 1, padding='same')(layer1x1)
+    layer3x3 = Conv2D(F3, 3, 1, padding='same')(layer1x1)
     layer3x3 = BatchNorm()(layer3x3)
     layer3x3 = act('relu')(layer3x3)
 
-    layer1x1 = Conv2D(F12, 4, 1, padding='same')(layer3x3)
+    layer1x1 = Conv2D(F12, 1, 1, padding='same')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)
 
     layer_out = add()([layer1x1, A_prev])
     layer_out = act('relu')(layer_out)
 
     return layer_out
+
