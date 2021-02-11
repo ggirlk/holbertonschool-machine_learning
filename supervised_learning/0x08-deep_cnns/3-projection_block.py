a01088d5995496fae583f22f7645cd2d97d9c515
= <ggirlk2@gmail.com>
2021-02-11 03:35:32 +0100

    Projection Block pycode

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -11,7 +11,7 @@ def projection_block(A_prev, filters, s=2):
     Add = K.layers.Add
     F11, F3, F12 = filters
 
-    layer1x1 = Conv2D(F11, 1, s,padding='same',
+    layer1x1 = Conv2D(F11, 1, s, padding='same',
                       kernel_initializer='he_normal')(A_prev)
     layer1x1 = BatchNorm()(layer1x1)
     layer1x1 = Activation('relu')(layer1x1)
@@ -21,11 +21,11 @@ def projection_block(A_prev, filters, s=2):
     layer3x3 = BatchNorm()(layer3x3)
     layer3x3 = Activation('relu')(layer3x3)
 
-    layer1x1 = Conv2D(F12, 1,padding='same',
+    layer1x1 = Conv2D(F12, 1, padding='same',
                       kernel_initializer='he_normal')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)  #
 
-    layer1x1_s = Conv2D(F12, 1, s,padding='same',
+    layer1x1_s = Conv2D(F12, 1, s, padding='same',
                         kernel_initializer='he_normal')(A_prev)
     layer1x1_s = BatchNorm()(layer1x1_s)  #
 
