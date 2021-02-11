22241924d3228677fbc095d6f4c862a6b730441a
= <ggirlk2@gmail.com>
2021-02-11 23:38:03 +0100

    -_-

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -5,5 +5,28 @@ import tensorflow.keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """ doc """
-    
-    return
+    MaxPooling2D = K.layers.MaxPooling2D
+    AveragePooling2D = K.layers.AveragePooling2D
+    Conv2D = K.layers.Conv2D
+    Dense = K.layers.Dense
+    BatchNorm = K.layers.BatchNormalization
+    Activation = K.layers.Activation
+    Concatenate = K.layers.Concatenate
+
+    def layersConv(X, k, f, p='valid', s=1):
+        layer = Conv2D(k, f, s, padding=p,
+                       kernel_initializer='he_normal')(X)
+        return layer
+
+    layer_prev = X
+    for layer in range(layers):
+        layer_new = BatchNorm()(layer_prev)
+        layer_new = Activation('relu')(layer_new)
+        layer_new = layersConv(layer_new, growth_rate, 1, 'same')
+        layer_new = BatchNorm()(layer_new)
+        layer_new = Activation('relu')(layer_new)
+        layer_new = layersConv(layer_new, growth_rate, 1, 'same')
+        
+        layer_prev = Concatenate()([layer_prev, layer_new])
+        nb_filters += growth_rate
+    return layer_prev, nb_filters
