751e99affb7cfc0793f5bd48eec1f5b3336ea451
= <ggirlk2@gmail.com>
2021-02-11 10:04:12 +0100

    -_-

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -25,7 +25,7 @@ def projection_block(A_prev, filters, s=2):
                       kernel_initializer='he_normal')(layer3x3)
     layer1x1 = BatchNorm()(layer1x1)  #
 
-    layer1x1_s = Conv2D(F12, 1, s, padding='same',
+    layer1x1_s = Conv2D(F12, 1, s,
                         kernel_initializer='he_normal')(A_prev)
     layer1x1_s = BatchNorm()(layer1x1_s)  #
 
