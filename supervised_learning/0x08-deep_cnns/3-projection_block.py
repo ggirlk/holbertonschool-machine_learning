8fba0c3e7fd4fb28d4711a327a274593182b30cd
= <ggirlk2@gmail.com>
2021-02-12 05:34:37 +0100

    ResNet50 try an other code

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -11,7 +11,7 @@ def projection_block(A_prev, filters, s=2):
     Add = K.layers.Add
     F11, F3, F12 = filters
 
-    layer1x1 = Conv2D(F11, 1, s,
+    layer1x1 = Conv2D(F11, 1, s, padding='same',
                       kernel_initializer='he_normal')(A_prev)
     layer1x1 = BatchNorm()(layer1x1)
     layer1x1 = Activation('relu')(layer1x1)
