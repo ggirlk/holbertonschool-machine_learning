f3f38fdbb173b5e6027b08433555a3a658c103f9
= <ggirlk2@gmail.com>
2021-03-17 12:11:11 +0100

    determinant

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -10,4 +10,25 @@ def determinant(matrix):
         if len(matrix) == 1 and len(matrix[0]) == 0:
                 return(1)
         raise ValueError("matrix must be a square matrix")
-    return 1
+    m = len(matrix)
+    det = 0
+
+    def copyMatrix(matrix):
+        """ copy list """
+        mat = []
+        for i in matrix:
+            d = []
+            for j in i:
+                d.append(j)
+            mat.append(d)
+        return mat
+
+    mat = copyMatrix(matrix)
+    for i in range(m):
+        for j in range(m):
+            del mat[j][i]
+        del mat[0]
+        det += -(-1)**i*matrix[0][i]*((mat[0][0]*mat[1][1])-(mat[0][1]*mat[1][0]))
+        mat = copyMatrix(matrix)
+
+    return det
