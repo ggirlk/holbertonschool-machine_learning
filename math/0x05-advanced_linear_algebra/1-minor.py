f561ee3b470219b2dcb9b561a3a2308788fb4069
= <ggirlk2@gmail.com>
2021-03-20 17:40:06 +0100

    minor

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -18,8 +18,11 @@ def determinant(matrix, n=0):
     if len(matrix) != len(matrix[0]):
         if len(matrix) == 1 and len(matrix[0]) == 0:
             return(1)
-        raise ValueError("matrix must be a square matrix")
+        raise ValueError("matrix must be a non-empty square matrix")
     n = len(matrix)
+    for i in range(n):
+        if type(matrix[i]) != list:
+            raise TypeError("matrix must be a list of lists")
     if n == 1:
         return matrix[0][0]
     if n == 2:
