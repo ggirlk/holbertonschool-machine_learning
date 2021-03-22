4389753aebf43512a233228669bbc46a589f8baf
= <ggirlk2@gmail.com>
2021-03-22 20:55:39 +0100

    Initialize

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -24,7 +24,10 @@ class MultiNormal():
     
     def __init__(self, data):
         """ constructor """
-        if type(data) is not np.ndarray:
+        if type(data) is not np.ndarray or len(data.shape) < 2:
             raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+
         self.mean = data.mean(axis=1, keepdims=True)
         _, self.cov = mean_cov(data.T)
