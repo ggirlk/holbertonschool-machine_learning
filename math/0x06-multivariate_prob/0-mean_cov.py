5953e7099930ab9ab9cb977648b9a0d2371fdbc5
= <ggirlk2@gmail.com>
2021-03-22 20:29:26 +0100

    correlation

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -3,28 +3,17 @@
 import numpy as np
 
 
-def mean_cov(X):
-    """
-    calculate the mean and
-    covariance of a data set
-    """
-    if type(X) != np.ndarray or (len(X.shape) != 2):
-        raise TypeError("X must be a 2D numpy.ndarray")
-    n = X.shape[0] - 1
-    if n < 2:
-        raise ValueError("X must contain multiple data points")
-    mean = X.mean(axis=0, keepdims=True)
-    x = X - mean
-    cov = np.dot(x.T, X.conj()) / n
-    return mean, cov
-
 def correlation(C):
     """
     calculate a correlation matrix
     """
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[0]:
+    if C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square")
-    _, cov = mean_cov(C)
-    return (np.corrcoef(C))
+    corr = np.ndarray((C.shape))
+    d = C.shape[0]
+    for i in range(d):
+        for j in range(d):
+            corr[i,j] = C[i,j]/(np.sqrt(C[i,i]*C[j,j]))
+    return corr
