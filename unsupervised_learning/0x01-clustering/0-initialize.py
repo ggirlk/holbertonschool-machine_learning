b6fc0e5aeb26b321a55f4250e38bcf47d6ad0af3
= <ggirlk2@gmail.com>
2021-04-01 13:57:58 +0100

    initialize

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,9 +7,9 @@ import numpy as np
 def initialize(X, k):
     """ doc """
     try:
-		n, d = X.shape
-        return np.random.uniform(X.min(axis=0),
-								 X.max(axis=0),
-								 size=(k, d))
+        n, d = X.shape
+        return np.random.uniform(np.amin(X, axis=0),
+                                 np.amax(X, axis=0),
+                                 size=(k, d))
     except Exception:
         return None
