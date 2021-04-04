0fb4c89bee57db8ca95b2667b712e9582d76321b
= <ggirlk2@gmail.com>
2021-04-04 05:48:11 +0100

    kmeans

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -2,14 +2,40 @@
 """ performs K-means on a dataset """
 
 import numpy as np
-from sklearn.cluster import KMeans
+
+
+def initialize(X, k):
+    """ doc """
+    try:
+        n, d = X.shape
+        if k == 0 or d == 0:
+            return None
+        return np.random.uniform(np.amin(X, axis=0),
+                                 np.amax(X, axis=0),
+                                 size=(k, d))
+    except Exception:
+        return None
 
 
 def kmeans(X, k, iterations=1000):
     """ doc """
     n, d = X.shape
-    init = np.random.uniform(np.amin(X, axis=0),
-                                 np.amax(X, axis=0),
-                                 size=(k, d))
-    km = KMeans(n_clusters=k, init=init, n_init=1, max_iter=iterations).fit(X)
-    return km.cluster_centers_, km.labels_
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+    C = initialize(X, k)
+    if C is None:
+        return None, None
+    clss = None
+    for itr in range(iterations):
+        prevC = C.copy()
+        clss = np.apply_along_axis(np.subtract, 1, X, C)
+        clss = np.argmin(np.square(clss).sum(axis=2), axis=1)
+        for cent in range(k):
+            Xs = np.argwhere(clss == cent)
+            if Xs.shape[0] == 0:
+                C[cent] = initialize(X, 1)
+            else:
+                C[cent] = np.mean(X[Xs], axis=0)
+        if np.all(prevC == C):
+            break;
+    return C, clss
