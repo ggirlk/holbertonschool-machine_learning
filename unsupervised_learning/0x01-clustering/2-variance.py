d0919702a24b8e183ac10434bd7d0c2a3cc0faaf
= <ggirlk2@gmail.com>
2021-04-05 02:16:57 +0100

    kmeans var

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -4,8 +4,10 @@
 import numpy as np
 
 
-
 def variance(X, C):
     """ doc """
-    sub = np.apply_along_axis(np.subtract, 1, X, C)
-    return ((sub)**2).sum(axis=2).min(axis=1).sum()
+    try:
+        sub = np.apply_along_axis(np.subtract, 1, X, C)
+        return ((sub)**2).sum(axis=2).min(axis=1).sum()
+    except Exception:
+        return None
