8cce5c7b4a9ad68d513406008fea40db7d605799
= <ggirlk2@gmail.com>
2021-04-05 06:12:59 +0100

    kmeans GMM

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -11,8 +11,8 @@ def initialize(X, k):
     if type(X) is not np.ndarray or X.ndim != 2\
        or type(k) is not int or k < 1:
         return None, None, None
+
     n, d = X.shape
-    
     pi = np.full((k,), 1/k)
     m, _ = kmeans(X, k)
     S = np.full((k, d, d), np.identity(d))
