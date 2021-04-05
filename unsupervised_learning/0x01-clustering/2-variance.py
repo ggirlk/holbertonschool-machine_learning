5de757da84163771ea84b7ae5a80f7995ee0d0ad
= <ggirlk2@gmail.com>
2021-04-05 02:28:13 +0100

    kmeans var

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -6,11 +6,11 @@ import numpy as np
 
 def variance(X, C):
     """ doc """
-    if len(X.shape) > 2 or type(X) is not np.ndarray\
+    if type(X) is not np.ndarray\
        or type(C) is not np.ndarray:
         return None
     try:
         sub = np.apply_along_axis(np.subtract, 1, X, C)
-        return ((sub)**2).sum(axis=2).min(axis=1).sum()
+        return ((sub)**2).sum(axis=axis=len(X.shape)).min(axis=1).sum()
     except Exception:
         return None
