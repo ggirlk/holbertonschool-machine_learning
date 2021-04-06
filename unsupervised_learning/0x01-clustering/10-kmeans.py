2f73258673a9275db97e7489be762cfed2bfa5bb
= <ggirlk2@gmail.com>
2021-04-07 00:56:19 +0100

    sklearn

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ performs K-means on a dataset """
 
-from sklearn.cluster import KMeans
+import sklearn.cluster as cl
 
 
 def kmeans(X, k, iterations=1000):
     """ doc """
     n, d = X.shape
-    km = KMeans(n_clusters=k, n_init=1, max_iter=iterations).fit(X)
+    km = cl.KMeans(n_clusters=k, n_init=1, max_iter=iterations).fit(X)
     return km.cluster_centers_, km.labels_
