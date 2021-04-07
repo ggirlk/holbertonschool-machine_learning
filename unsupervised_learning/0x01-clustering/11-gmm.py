536d977e45975ce87a9fd676dd40a557140cb39c
= <ggirlk2@gmail.com>
2021-04-07 01:22:21 +0100

    sklearn

---
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" performs K-means on a dataset """
+
+import sklearn.mixture.GaussianMixture as gm
+
+
+def gmm(X, k):
+    """ doc """
+    n, d = X.shape
+    gmm = sm.GaussianMixture(n_components=k).fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+    return pi, m, S, clss, bic
