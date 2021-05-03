f7d01758986f5c830b029eb8b29ee40ff9216bae
= <ggirlk2@gmail.com>
2021-05-03 04:37:39 +0100

    e_m

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -13,15 +13,17 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         pi, m, S = initialize(X, k)
         i = 0
         prevl = 0
+        g, l = expectation(X, pi, m, S)
         while i < iterations:
-            g, l = expectation(X, pi, m, S)
             pi, m, S = maximization(X, g)
-            if verbose and (not i%10 or  round(prevl, 5) == round(l, 5)):
+            if verbose and (not i%10 or round(prevl, 5) == round(l, 5)):
                 print("Log Likelihood after {} iterations: {}".format(i, round(l, 5)))
-            if l > tol or prevl == l:
+            if l >= tol or round(prevl, 5) == round(l, 5):
                 return pi, m, S, g, l
-            i += 1
             prevl = l
+            g, l = expectation(X, pi, m, S)
+            i += 1
+            
         return pi, m, S, g, l
     except Exception:
         return None, None, None, None, None
