48f07c84c82409365244a878cdcc45fddf2ea2dc
= <ggirlk2@gmail.com>
2021-06-06 03:35:36 +0100

    TF-IDF

---
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Bag Of Words """
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+bag_of_words = __import__('0-bag_of_words').bag_of_words
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    ***********************************************
+    ********** creates a TF-IDF embedding *********
+    ***********************************************
+    @sentences: is a list of sentences to analyze
+    @vocab: is a list of the vocabulary words to use
+            for the analysis
+            **If None: all words within sentences
+                       should be used
+    Returns: embeddings, features
+             embeddings: is a numpy.ndarray of shape (s, f)
+             containing the embeddings
+                 s is the number of sentences in sentences
+                 f is the number of features analyzed
+             features: is a list of the features used for embeddings
+    """
+    E, vocab = bag_of_words(sentences, vocab)
+    return TfidfTransformer().fit_transform(E).toarray(), vocab
