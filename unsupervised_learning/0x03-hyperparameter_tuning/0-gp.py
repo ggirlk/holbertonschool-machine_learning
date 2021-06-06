c3a83e39f04f1f3d94baaedc3f0832ec62c7b191
= <ggirlk2@gmail.com>
2021-06-06 06:57:17 +0100

    Initialize Gaussian Process

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -6,7 +6,7 @@ import numpy as np
 class GaussianProcess():
     """ noiseless 1D Gaussian process """
 
-    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, lk=1, sigma_f=1):
         """
         *************************************************
         ***************** constructor *******************
@@ -18,13 +18,13 @@ class GaussianProcess():
                  representing the outputs of the black-box
                  function for each input in X_init
         @t is: the number of initial samples
-        @l is: the length parameter for the kernel
+        @lk is: the length parameter for the kernel
         @sigma_f: is the standard deviation given to the
                   output of the black-box function
         """
         self.X = X_init
         self.Y = Y_init
-        self.l = l
+        self.l = lk
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
