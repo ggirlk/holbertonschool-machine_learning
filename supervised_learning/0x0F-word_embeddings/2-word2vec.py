2bf5045ede182b4d4872c8453ac17079d76d81a4
= <ggirlk2@gmail.com>
2021-06-06 04:58:16 +0100

    Train Word2Vec

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -4,7 +4,7 @@ from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, size=100, min_count=5, window=5,
-                       negative=5, cbow=True, iterations=5, seed=0, workers=1):
+                   negative=5, cbow=True, iterations=5, seed=0, workers=1):
     """
     ************************************************
     ** creates and trains a gensim word2vec model **
