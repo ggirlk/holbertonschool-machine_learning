8fbfecc2ff87226581da7f8e32ae9ff5295c144f
= <ggirlk2@gmail.com>
2021-06-06 05:30:16 +0100

    FastText

--- supervised_learning/0x0F-word_embeddings/m.py
+++ supervised_learning/0x0F-word_embeddings/m.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from gensim.test.utils import common_texts
-word2vec_model = __import__('2-word2vec').word2vec_model
-gensim_to_keras = __import__('3-gensim_to_keras').gensim_to_keras
+fasttext_model = __import__('4-fasttext').fasttext_model
 
 print(common_texts[:2])
-w2v = word2vec_model(common_texts, min_count=1)
-print(gensim_to_keras(w2v))
+ft = fasttext_model(common_texts, min_count=1)
+print(ft.wv["computer"])
