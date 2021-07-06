98e8140ffb9715607743d97b80aa84d191973b79
= <ggirlk2@gmail.com>
2021-07-06 20:12:47 +0100

    update script

--- reinforcement_learning/0x01-deep_q_learning/play.py
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -1,5 +1,4 @@
 from __future__ import division
-import argparse
 
 from PIL import Image
 import numpy as np
@@ -11,7 +10,7 @@ from keras.optimizers import Adam
 import keras.backend as K
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy, GreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
@@ -42,7 +41,7 @@ class AtariProcessor(Processor):
 
 class args:
     env_name = "Breakout-v0" # 'BreakoutDeterministic-v4'
-    mode = 'play'
+    mode = 'train'
     weights = None
 
 # Get the environment and extract the number of actions.
@@ -54,33 +53,37 @@ nb_actions = env.action_space.n
 # Next, we build our model. We use the same model that was described by Mnih et al. (2015).
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 
+model = Sequential()
+if K.image_dim_ordering() == 'tf':
+    # (width, height, channels)
+    model.add(Permute((2, 3, 1), input_shape=input_shape))
+elif K.image_dim_ordering() == 'th':
+    # (channels, width, height)
+    model.add(Permute((1, 2, 3), input_shape=input_shape))
+else:
+    raise RuntimeError('Unknown image_dim_ordering.')
 
-def create_q_model():
-    # Network defined by the Deepmind paper
-    inputs = Input(shape=(84, 84, 4,))
-
-    # Convolutions on the frames on the screen
-    layer1 = Conv2D(32, 8, strides=4, activation="relu")(inputs)
-    layer2 = Conv2D(64, 4, strides=2, activation="relu")(layer1)
-    layer3 = Conv2D(64, 3, strides=1, activation="relu")(layer2)
-
-    layer4 = Flatten()(layer3)
-
-    layer5 = Dense(512, activation="relu")(layer4)
-    action = Dense(nb_actions, activation="linear")(layer5)
-
-    return Model(inputs=inputs, outputs=action)
-
-model = create_q_model()
+model.add(Conv2D(32, (8, 8), strides=(4, 4)))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (4, 4), strides=(2, 2)))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), strides=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
 
 print(model.summary())
 
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 processor = AtariProcessor()
 
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+"""policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
+                              nb_steps=1000000)"""
 
+policy = GreedyQPolicy()
 
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
--- reinforcement_learning/0x01-deep_q_learning/train.py
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -1,5 +1,4 @@
 from __future__ import division
-import argparse
 
 from PIL import Image
 import numpy as np
@@ -11,7 +10,7 @@ from keras.optimizers import Adam
 import keras.backend as K
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy, GreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
@@ -54,47 +53,50 @@ nb_actions = env.action_space.n
 # Next, we build our model. We use the same model that was described by Mnih et al. (2015).
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 
+model = Sequential()
+if K.image_dim_ordering() == 'tf':
+    # (width, height, channels)
+    model.add(Permute((2, 3, 1), input_shape=input_shape))
+elif K.image_dim_ordering() == 'th':
+    # (channels, width, height)
+    model.add(Permute((1, 2, 3), input_shape=input_shape))
+else:
+    raise RuntimeError('Unknown image_dim_ordering.')
 
-def create_q_model():
-    # Network defined by the Deepmind paper
-    inputs = Input(shape=(84, 84, 4,))
-
-    # Convolutions on the frames on the screen
-    layer1 = Conv2D(32, 8, strides=4, activation="relu")(inputs)
-    layer2 = Conv2D(64, 4, strides=2, activation="relu")(layer1)
-    layer3 = Conv2D(64, 3, strides=1, activation="relu")(layer2)
-
-    layer4 = Flatten()(layer3)
-
-    layer5 = Dense(512, activation="relu")(layer4)
-    action = Dense(nb_actions, activation="linear")(layer5)
-
-    return Model(inputs=inputs, outputs=action)
-
-model = create_q_model()
+model.add(Conv2D(32, (8, 8), strides=(4, 4)))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (4, 4), strides=(2, 2)))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), strides=(1, 1)))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
 
 print(model.summary())
 
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 processor = AtariProcessor()
 
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+"""policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
+                              nb_steps=1000000)"""
 
+policy = GreedyQPolicy()
 
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
                processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
                train_interval=4,
                delta_clip=1.)
 
-
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
 
 weights_filename = 'policy.h5f'
 
-checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
-log_filename = 'dqn_{}_log.json'.format(args.env_name)
+checkpoint_weights_filename = 'weights.h5f'
+log_filename = 'log.json'
 
 callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
 callbacks += [FileLogger(log_filename, interval=100)]
