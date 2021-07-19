8f93c2b41943db1794bafaf20c0283f32fd39721
= <ggirlk2@gmail.com>
2021-07-19 09:15:09 +0100

    Monte Carlo Poliy Gradien

---
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Policy Gradients """
+import numpy as np
+
+
+def policy(state, weight):
+    """
+    ****************************************************
+    ****computes to policy with a weight of a matrix****
+    ****************************************************
+    @state: matrix representing the current
+            observation of the environment
+    @weight: matrix of random weight
+    Return: weighted policy
+    """
+    # calculate using softmax
+    z = np.dot(state, weight)
+    ez = np.exp(z)
+    ez /= ez.sum()
+    return ez
+
+
+def policy_gradient(state, weight):
+    """
+    ****************************************************
+    ******computes the Monte-Carlo policy gradient******
+    ********based on a state and a weight matrix********
+    ****************************************************
+    @state: matrix representing the current
+            observation of the environment
+    @weight: matrix of random weight
+    """
+    state = state.reshape(1, -1)
+    policy_value = policy(state, weight)
+    action = np.random.choice((policy_value[0]).shape[0], p=policy_value[0])
+    # ∇lnπ(At|St,θ) 
+    # left = (∇θ)log(e^(ϕ(s,a).⊺*θ)) = (∇θ)ϕ(s,a).⊺*θ = ϕ(s,a)
+    # right = ∑k=1toN (ϕ(s,ak) * (πθ)(s,ak))
+    # (∇θ)log((πθ)(s,a)) = left − right = ϕ(s,a) − (Eπθ)[ϕ(s,⋅)]
+    grad = state.T - (state.T * policy_value[:, None]).sum(axis=0)
+    return action, grad
---
+++ reinforcement_learning/0x03-policy_gradients/train.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+""" Policy Gradients """
+import numpy as np
+
+from policy_gradient import policy_gradient
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
+    """
+    *********************************************
+    ******Implementation of a full training******
+    *********************************************
+    @env: initial environment
+    @nb_episodes: number of episodes used for training
+    @alpha: the learning rate
+    @gamma: the discount factor
+    Return:
+        all values of the score (sum of all rewards
+        during one episode loop)
+    """
+    # Inisiate scores list
+    scores = []
+    # Initiate θ to random
+    # np.random.seed(0)
+    # env.seed(0)
+    W = np.random.rand(env.observation_space.shape[0],
+                       env.action_space.n)
+    # W = np.zeros((env.observation_space.shape[0],
+    #                          env.action_space.n))
+    for ep in range(nb_episodes):
+        # Generating episode
+        # Reseting the environment each time as per requirement
+        state = env.reset()[None,:]
+        # initiate needed variabes
+        done = False
+        t = 0
+        R = []
+        Grads = []
+        Actions = []
+        while not done:
+            # Renderig the environment every 1000 
+            if show_result and not ep % 1000:
+                env.render()
+            # taking action
+            action, grad = policy_gradient(state, W)
+            # Taking the action and getting the reward and outcome state
+            new_state, Returns, done, info = env.step(action)
+            # Appending needed Values
+            Actions.append(action)
+            R.append(Returns)
+            Grads.append(grad)
+            state = new_state[None,:]
+            t += 1
+        # Appending summed score
+        scores.append(sum(R))
+        print("Episode N°: "+ str(ep) +" Score: "+ str(sum(R)), end="\r", flush=False)
+
+        # Updating θ
+        # initiate needed variabes
+        G = 0  # empirical return
+        T = t
+        for t in range(T):
+            Returns = R[t]
+            action = Actions[t]
+            # Gt = ∑k=0 to ∞ (γ^(k) * R(t+k+1))
+            G = sum(gamma**(k) * R[k+t+1] for k in range(T-t-1))
+            # θ ← θ + α * γ^(t) * Gt * ∇θlnπθ(At|St) ; from Barto Satton book
+            # W[:, action] += alpha * Grads[t][:, action] * gamma**(t) * G
+            # θ ← θ + α * ∇θlogπθ(st, at) * vt ; from David Silver course
+            W[:, action] += alpha * Grads[t][:, action] * G 
+
+    return scores
