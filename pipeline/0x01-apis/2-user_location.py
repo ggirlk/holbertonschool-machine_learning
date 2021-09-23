dab62d78f6400c117781dbbeb657cefd3d7e389c
khouloud <ggirlk2@gmail.com>
2021-09-23 14:52:55 +0100

    Rate me is you can!

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -4,6 +4,7 @@ prints the location of a specific user
 """
 import requests
 import sys
+import time
 
 
 if __name__ == '__main__':
@@ -15,7 +16,12 @@ if __name__ == '__main__':
         if r.status_code == 404:
             print("Not found")
         if r.status_code == 403:
-            print("403")
+            reset = r.headers['X-Ratelimit-Reset']
+            reset = int(reset) - int(time.time())
+            print("Reset in {} min".format(int(reset / 60)))
+        else:
+            print("Not found")
         if r.status_code == 200:
             json = r.json()
             print(json["location"])
+        
